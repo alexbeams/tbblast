d0fe5cf2147b476f51f578fba98a823f93ce81c2
Alex Beams <abeams@sfu.ca>
2025-02-03 16:40:42 -0800

    correlate clusters with lbi

--- snp_clusters.R
+++ snp_clusters.R
@@ -1,5 +1,7 @@
-# find the bandwidths in LBI that maximize the signal-to-noise ratio across the posterior distribution of trees for each lineage.
+# find the bandwidths in LBI that maximize the signal-to-noise ratio across the posterior distribution of trees for each lineage. If you don't do this, then LBI is going to be conveying noise from uncertainty in the phylogenetic relationships, rather than a meaningful signal.
 
+# then we find the SNP threshold for clustering that establishes the strongest correlation between LBI and
+# cluster size. The clusters defined by this SNP threshold are informed by the phylogenies. 
 
 require(Biostrings)
 require(ape)
@@ -7,104 +9,6 @@ require(treeio)
 
 source("lbi.R") #to calculate LBI
 
-##load in the alignments - these are just the variable sites:
-#align1 <- readDNAStringSet('lineage_files/alignment_lineage1.fasta')
-#align2 <- readDNAStringSet('lineage_files/alignment_lineage2.fasta')
-#align3 <- readDNAStringSet('lineage_files/alignment_lineage3.fasta')
-#align4 <- readDNAStringSet('lineage_files/alignment_lineage4.fasta')
-#
-## compare with the full alignment - check that we didn't mess up the lineage
-## files when we split them up into different lineages:
-#align.full <- readDNAStringSet('Malawi_final_filtered.fasta')
-#
-##convert these to matrices:
-#align1mat <- as.matrix(align1)
-#align2mat <- as.matrix(align2)
-#align3mat <- as.matrix(align3)
-#align4mat <- as.matrix(align4)
-#
-## calculate the pairwise SNP distances for alignment 1:
-#align1dists <- matrix(nrow=nrow(align1mat),ncol=nrow(align1mat))
-#
-## just write a dumb nested for loop to fill in the matrix (it's symmetric): 
-#for(i in 1:nrow(align1mat)){
-#	for(j in i:nrow(align1mat)){
-#		align1dists[i,j] <- sum(align1mat[i,] != align1mat[j,])
-#		align1dists[j,i] <- align1dists[i,j]	
-#	}
-#}
-#
-## alignment 2:
-#align2dists <- matrix(nrow=nrow(align2mat),ncol=nrow(align2mat))
-#
-## just write a dumb nested for loop to fill in the matrix (it's symmetric): 
-#for(i in 1:nrow(align2mat)){
-#	for(j in i:nrow(align2mat)){
-#		align2dists[i,j] <- sum(align2mat[i,] != align2mat[j,])
-#		align2dists[j,i] <- align2dists[i,j]	
-#	}
-#}
-#
-#
-#align3dists <- matrix(nrow=nrow(align3mat),ncol=nrow(align3mat))
-#
-## just write a dumb nested for loop to fill in the matrix (it's symmetric): 
-#for(i in 1:nrow(align3mat)){
-#	for(j in i:nrow(align3mat)){
-#		align3dists[i,j] <- sum(align3mat[i,] != align3mat[j,])
-#		align3dists[j,i] <- align3dists[i,j]	
-#	}
-#}
-#
-#align4dists <- matrix(nrow=nrow(align4mat),ncol=nrow(align4mat))
-#
-## just write a dumb nested for loop to fill in the matrix (it's symmetric): 
-#for(i in 1:nrow(align4mat)){
-#	for(j in i:nrow(align4mat)){
-#		align4dists[i,j] <- sum(align4mat[i,] != align4mat[j,])
-#		align4dists[j,i] <- align4dists[i,j]	
-#	}
-#}
-#
-## compare with the function built in to Biostrings (appears to match, but this one runs much faster):
-#align1dists.compare <-  pwalign::stringDist(align1,method='hamming')
-#align2dists.compare <-  pwalign::stringDist(align2,method='hamming')
-#align3dists.compare <-  pwalign::stringDist(align3,method='hamming')
-#align4dists.compare <-  pwalign::stringDist(align4,method='hamming')
-#
-## look at the full alignment
-#aligndists.compare <- pwalign::stringDist(align.full,method='hamming')
-# 
-## the minimum hamming distances look pretty large:
-#min(align1dists.compare)
-#min(align2dists.compare)
-#min(align3dists.compare)
-#min(align4dists.compare)
-#
-## indeed, the observed min of 31 is also true for the original alignment
-#min(aligndists.compare)
-#
-## what about if we examine sequences collected from the same individuals at different times?
-#align.allsequences <- readDNAStringSet('Malawi_final.fasta')
-#
-#align.withinhost <- setdiff(align.allsequences,align.full)
-#align.withinhost.compare <- pwalign::stringDist(align.withinhost,method='hamming')
-#
-## distribution of distances looks interesting, esp. for lineage 4:
-#par(mfrow=c(2,2))
-#hist(align1dists.compare,breaks=50)
-#hist(align2dists.compare)
-#hist(align3dists.compare)
-#hist(align4dists.compare,breaks=100)
-#
-## so it looks like there are 3 distinct groups within lineage 4, 2 distinct groups within lineage 1 
-#
-#heatmap(as.matrix(align1dists.compare), symm=T)
-#heatmap(as.matrix(align2dists.compare), symm=T)
-#heatmap(as.matrix(align3dists.compare), symm=T)
-#heatmap(as.matrix(align4dists.compare), symm=T)
-
-# everything prior to this treats ambiguous sites as nucleotides; let's use the ape 
 # functions that treat ambiguous sites as missing data (?)
 
 align1 <- read.FASTA('lineage_files/alignment_lineage1.fasta')
@@ -121,7 +25,20 @@ align4.dist <- dist.dna(align4,model='N')
 align1.clusters <- cutree(hclust(align1.dist),h=12)
 align2.clusters <- cutree(hclust(align2.dist),h=12)
 align3.clusters <- cutree(hclust(align3.dist),h=12)
-align4.clusters <- cutree(hclust(align4.dist),h=12)
+align4.clusters <- cutree(hclust(align4.dist),h=49)
+
+getPlot <- function(cutoff,align.dist,dat){
+
+	align.clusters <- cutree(hclust(align.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align.clusters), snpcluster = align.clusters)
+	df = merge(dat, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	plot(lbi~as.double(snpclustersize),df)
+}
+
+
+
 
 # probably need to read in the posterior distributions of trees in here:
 # refer to the bissemodels.R file
@@ -162,6 +79,11 @@ var(apply(align1.lbi,1,mean))
 
 var(apply(align1.lbi,1,mean)) / mean(apply(align1.lbi,1,var))
 
+# we want to maximize the F-test statistic over tau. Each tip is a group of observations with
+# as many observations as trees in the posterior sample; all groups have equal size, and
+# the degrees of freedom (numerator and denominator) are constant wrt tau, so this function
+# works fine for finding the optimal tau, although it isn't actually equal to the F-statistic 
+
 getF.align1 <- function(tau){
 	align1.lbi = sapply(phi1_sample,function(x) lbi(x,tau))
 	align1.lbi = align1.lbi[1:99,]	
@@ -172,7 +94,7 @@ getF.align1 <- function(tau){
 tauvals.align1 <- seq(.005,.01,length=15)
 F.align1 <- sapply(tauvals.align1, getF.align1)
 plot(tauvals.align1,F.align1,type='l',xlab=bquote(tau),ylab='F',
-	main='Alignment 1 LBI signal')
+	main='Lineage 1 LBI signal')
 
 # lineage 2: two clear groups distinguished:
 tau2 <- 5.25e-4
@@ -263,16 +185,16 @@ plot(tauvals.align4,F.align4,type='l',xlab=bquote(tau),ylab='F',
 
 par(mfrow=c(2,2))
 plot(tauvals.align1,F.align1,type='l',xlab=bquote(tau),ylab='F',
-	main='Alignment 1 LBI signal-to-noise')
+	main='Lineage 1 LBI signal-to-noise')
 
 plot(tauvals.align2,F.align2,type='l',xlab=bquote(tau),ylab='F',
-	main='Alignment 2 LBI signal-to-noise')
+	main='Lineage 2 LBI signal-to-noise')
 
 plot(tauvals.align3,F.align3,type='l',xlab=bquote(tau),ylab='F',
-	main='Alignment 3 LBI signal-to-noise')
+	main='Lineage 3 LBI signal-to-noise')
 
 plot(tauvals.align4,F.align4,type='l',xlab=bquote(tau),ylab='F',
-	main='Alignment 4 LBI signal-to-noise')
+	main='Lineage 4 LBI signal-to-noise')
 
 par(mfrow=c(2,2))
 
@@ -304,5 +226,217 @@ plot(align4.lbi.mean[1:513],align4.lbi.mean[1:513],
         main=bquote(tau[4]==.(tau4)))
 for(i in 1:100){points(align4.lbi.mean[1:513],align4.lbi[1:513,i])}
 points(align4.lbi.mean[1:513],align4.lbi.mean[1:513],col='red')
+
+# need to read in dat, separate into dat1, dat2, dat3, dat4, then:
+
+crud= data.frame(Sequence_name=names(align4.clusters), snpcluster = align4.clusters)
+df = merge(dat4, crud)
+df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+plot(lbi~as.double(snpclustersize),df)
+
+# what SNP cutoff establishes the clearest relationship between clustersize and lbi?
+# same trick as before: calculate E[ lbi | clustersize ], and choose cutoff so that
+# Var[ E[lbi|clustersize]] >> E[Var[lbi|clustersize]]
+# for this, we do want to be careful about correctly calculating Snedecor's F, because
+# the numbers in each group of clustersize will change with the threshold
+# we use the aov function this time:
+
+getF.snpclustersize.align1 <- function(cutoff){
+
+	align1.clusters <- cutree(hclust(align1.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align1.clusters), snpcluster = align1.clusters)
+	df = merge(dat1, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	aovmod <- aov(lbi~as.double(snpclustersize),df)
+	F.snpclustersize <- summary(aovmod)[[1]][["F value"]][[1]]
+	p.snpclustersize <- summary(aovmod)[[1]][["Pr(>F)"]][[1]]
+		
+	return(c(F.snpclustersize, p.snpclustersize))
+
+}
+
+getF.snpclustersize.align2 <- function(cutoff){
+
+	align2.clusters <- cutree(hclust(align2.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align2.clusters), snpcluster = align2.clusters)
+	df = merge(dat2, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	aovmod <- aov(lbi~as.double(snpclustersize),df)
+	F.snpclustersize <- summary(aovmod)[[1]][["F value"]][[1]]
+	p.snpclustersize <- summary(aovmod)[[1]][["Pr(>F)"]][[1]]
+		
+	return(c(F.snpclustersize, p.snpclustersize))
+
+}
+
+getF.snpclustersize.align3 <- function(cutoff){
+
+	align3.clusters <- cutree(hclust(align3.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align3.clusters), snpcluster = align3.clusters)
+	df = merge(dat3, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	aovmod <- aov(lbi~as.double(snpclustersize),df)
+	F.snpclustersize <- summary(aovmod)[[1]][["F value"]][[1]]
+	p.snpclustersize <- summary(aovmod)[[1]][["Pr(>F)"]][[1]]
+		
+	return(c(F.snpclustersize, p.snpclustersize))
+
+}
+
+
+getF.snpclustersize.align4 <- function(cutoff){
+
+	align4.clusters <- cutree(hclust(align4.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align4.clusters), snpcluster = align4.clusters)
+	df = merge(dat4, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	aovmod <- aov(lbi~as.double(snpclustersize),df)
+	F.snpclustersize <- summary(aovmod)[[1]][["F value"]][[1]]
+	p.snpclustersize <- summary(aovmod)[[1]][["Pr(>F)"]][[1]]
+		
+	return(c(F.snpclustersize, p.snpclustersize))
+
+}
+
+cutoffs <- 10:100
+F.snpclustersizes.align1 <- sapply(cutoffs, getF.snpclustersize.align1)
+par(mfrow=c(2,1))
+plot(cutoffs,F.snpclustersizes.align1[1,],xlab='SNP threshold', ylab='F',main='Lineage 1')
+plot(cutoffs,log(F.snpclustersizes.align1[2,]),xlab='SNP threshold', ylab='log p-value')
+
+cutoffs <- 10:100
+F.snpclustersizes.align2 <- sapply(cutoffs, getF.snpclustersize.align2)
+par(mfrow=c(2,1))
+plot(cutoffs,F.snpclustersizes.align2[1,],xlab='SNP threshold', ylab='F',main='Lineage 2')
+plot(cutoffs,log(F.snpclustersizes.align2[2,]),xlab='SNP threshold', ylab='log p-value')
+
+cutoffs <- 10:100
+F.snpclustersizes.align3 <- sapply(cutoffs, getF.snpclustersize.align3)
+par(mfrow=c(2,1))
+plot(cutoffs,F.snpclustersizes.align3[1,],xlab='SNP threshold', ylab='F',main='Lineage 3')
+plot(cutoffs,log(F.snpclustersizes.align3[2,]),xlab='SNP threshold', ylab='log p-value')
+
+cutoffs <- 10:100
+F.snpclustersizes.align4 <- sapply(cutoffs, getF.snpclustersize.align4)
+par(mfrow=c(2,1))
+plot(cutoffs,F.snpclustersizes.align4[1,],xlab='SNP threshold', ylab='F',main='Lineage 4')
+plot(cutoffs,log(F.snpclustersizes.align4[2,]),xlab='SNP threshold', ylab='log p-value')
+
+# lineage 1: looks kind of pathological. LBI seems to just single out the distance sequences from the rest
+# lineage 2: SNP thresholds of 15-26 establish the strongest correlation with LBI
+# lineage 3: similar to lineage 1, looks pathological.  
+# lineage 4: looks like any cutoff between 32 and 50, inclusive, establishes the strongest relationsip with LBI for lineage 4
+
+#need to relabel the names of the tips in the tree according to the scheme in df (without dates)
+
+plotLbiClust1 <- function(treeindex,cutoff){
+	align1.clusters <- cutree(hclust(align1.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align1.clusters), snpcluster = align1.clusters)
+	df = merge(dat1, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	nms <- sapply(df$Sequence_name, function(x) grep(x, phi1_sample[[treeindex]]$tip.label)  )
+	nms <- cbind(names(nms), nms)
+	nms <- nms[order(as.numeric(nms[,2])),]
+
+	crud <- phi1_sample[[treeindex]]
+	crud$tip.label <- nms[,1]
+
+	p.tree <- ggtree(crud,layout='circular')
+
+	p.tree <- p.tree %<+% df
+
+	p.tree + geom_tippoint(aes(color=as.factor(snpclustersize),alpha=lbi))
+
+}
+
+plotLbiClust2 <- function(treeindex,cutoff){
+	align2.clusters <- cutree(hclust(align2.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align2.clusters), snpcluster = align2.clusters)
+	df = merge(dat2, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	nms <- sapply(df$Sequence_name, function(x) grep(x, phi2_sample[[treeindex]]$tip.label)  )
+	nms <- cbind(names(nms), nms)
+	nms <- nms[order(as.numeric(nms[,2])),]
+
+	crud <- phi2_sample[[treeindex]]
+	crud$tip.label <- nms[,1]
+
+	p.tree <- ggtree(crud,layout='circular')
+
+	p.tree <- p.tree %<+% df
+
+	p.tree + geom_tippoint(aes(color=as.factor(snpclustersize),alpha=lbi))
+
+}
+
+plotLbiClust3 <- function(treeindex,cutoff){
+	align3.clusters <- cutree(hclust(align3.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align3.clusters), snpcluster = align3.clusters)
+	df = merge(dat3, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	nms <- sapply(df$Sequence_name, function(x) grep(x, phi3_sample[[treeindex]]$tip.label)  )
+	nms <- cbind(names(nms), nms)
+	nms <- nms[order(as.numeric(nms[,2])),]
+
+	crud <- phi3_sample[[treeindex]]
+	crud$tip.label <- nms[,1]
+
+	p.tree <- ggtree(crud,layout='circular')
+
+	p.tree <- p.tree %<+% df
+
+	p.tree + geom_tippoint(aes(color=as.factor(snpclustersize),alpha=lbi))
+
+}
+
+plotLbiClust4 <- function(treeindex,cutoff){
+	align4.clusters <- cutree(hclust(align4.dist),h=cutoff)
+	crud= data.frame(Sequence_name=names(align4.clusters), snpcluster = align4.clusters)
+	df = merge(dat4, crud)
+	df$snpclustersize = table(df$snpcluster)[df$snpcluster]
+
+	nms <- sapply(df$Sequence_name, function(x) grep(x, phi4_sample[[treeindex]]$tip.label)  )
+	nms <- cbind(names(nms), nms)
+	nms <- nms[order(as.numeric(nms[,2])),]
+
+	crud <- phi4_sample[[treeindex]]
+	crud$tip.label <- nms[,1]
+
+	p.tree <- ggtree(crud,layout='circular')
+
+	p.tree <- p.tree %<+% df
+
+	p.tree + geom_tippoint(aes(color=as.factor(snpclustersize),alpha=lbi))
+
+}
+
+# what is the distribution of phylogenetic distances between sequences in the largest clusters?
+
+plotDists <- function(snpclustersize,treeindex){
+	nms <- sapply(df$Sequence_name, function(x) grep(x, phi4_sample[[treeindex]]$tip.label)  )
+	nms <- cbind(names(nms), nms)
+	nms <- nms[order(as.numeric(nms[,2])),]
+
+	crud <- phi4_sample[[treeindex]]
+	crud$tip.label <- nms[,1]
+
+	# extract the pairwise distances between the sequences in the cluster
+	dfcrud = df[df$snpclustersize==snpclustersize,]
+	clusternms = dfcrud$Sequence_name
+	
+	x = cophenetic(crud)
+	x=x[clusternms,clusternms]
+	
+	y = x[upper.tri(x)]
+	hist(y)
+}
 
 
